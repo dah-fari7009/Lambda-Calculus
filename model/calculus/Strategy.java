fb07b101146ac9595bd4353c6c19de9bf8bf98e7
farid <faridath.akinotcho@yahoo.fr>
2019-11-16 10:35:53 -0800

    Clean Up + command line

--- model/calculus/Strategy.java
+++ model/calculus/Strategy.java
@@ -1,62 +1,6 @@
 package model.calculus;
 
 import java.util.ArrayList;
-/*abstract class Strategy{
-
-  public Redexes reduce(LambdaTerm term){
-    if(term instanceof Variable) return reduceVar((Variable)term);
-    if(term instanceof Abstraction) return reduceAbs((Abstraction)term);
-    return reduceApp((Application)term);
-  }
-
-  public  Redexes reduceVar(Variable var){
-    return new Redexes(var,var);
-  }
-
-  public Redexes reduceAbs(Abstraction abs){
-      return new Redexes(abs,new Abstraction(abs.binding(),reduce(abs.body())));
-  }
-
-  public abstract LambdaTerm reduceApp(Application app);
-
-}
-
-class StrategyAOR extends Strategy{
-
-  public Redexes reduceApp(Application app){
-    if(app.func().isInBnf() && app.arg().isInBnf()){
-      Abstraction a=(Abstraction)app.func();
-      return new Redexes(app,a.substitute(a.binding(),app.arg()).body());
-    }
-    else if(app.func().isInBnf()) return new Redexes(app,new Application(app.func(),reduce(app.arg())));
-    else return new Redexes(app,new Application(reduce(app.func()),app.arg()));
-
-
-  }
-
-  public String toString(){
-    return ("Call-by-value Reduction (AOR)");
-  }
-
-}
-
-class StrategyNOR extends Strategy{
-
-  public Redexes reduceApp(Application app){
-    if(app.func() instanceof Abstraction){
-      Abstraction a=(Abstraction)app.func();
-      return new Redexes(app,a.substitute(a.binding(),app.arg()).body());
-    }
-    return new Redexes(app,new Application(reduce(app.func()),reduce(app.arg())));
-  }
-
-  public String toString(){
-    return ("Call-by-name Reduction (NOR)");
-  }
-}
-
-*/
-
 
 abstract class Strategy{
 
